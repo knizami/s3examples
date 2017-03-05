05497d4112b1a1f7063d424b0c64616953003627
Khurram Nizami <knizami@gmail.com>
2017-03-05 00:58:42 -0600

    checks for bucket existence Requires s3:ListMyBuckets permission

--- s3ex1.py
+++ s3ex1.py
@@ -1,10 +1,42 @@
 import boto3
+import sys
+import os.path
 from botocore.client import Config
 
+print """
+Will upload multiple files, skips a file that doesn't exist
+"""
+
 # Other valid options here are 'auto' (default) and 'virtual'
+
+if len(sys.argv) < 3:
+    print "usage: s3ex1.py <bucket> <files...>"
+    exit(1)
+
 session = boto3.Session(profile_name='dev')
 s3 = session.client('s3', 'us-west-2',
                     config=Config(s3={'addressing_style': 'path'}))
-s3.upload_file("/Users/khurramnizami/tmp.txt",
-               "s3examples", "/test/tmp_file")
-print "upload complete"
+
+
+response = s3.list_buckets()
+
+for checkbuck in response['Buckets']:
+    if checkbuck['Name'] == sys.argv[1]:
+        break
+else:
+    print "Bucket ", sys.argv[1], " doesn't exist, exiting.."
+    exit(1)
+
+
+uploaded = list()
+for fup in sys.argv[1:]:
+    if not os.path.isfile(fup):
+        print "file ", fup, " doesn't exist, skipping..."
+        continue
+    print "now uploading ", fup, "..."
+    s3.upload_file(fup,
+                   "s3examples", fup)
+    print "upload complete"
+    uploaded.append(fup)
+
+print "uploaded ", len(uploaded), " files: ", ",".join(str(x) for x in uploaded)
